9353569033ffa0982cf9d90a44d1d701d81a5f00
Thytu <valentin.de-matos@epitech.eu>
2022-01-01 18:32:33 +0100

    refactor: adv attacks now take **kwargs in param

--- src/adversarial.py
+++ src/adversarial.py
@@ -1,5 +1,7 @@
-import re
-from PIL.Image import NONE
+"""
+TODO
+"""
+
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
@@ -15,7 +17,7 @@ def _create_empty_mask(tensor: torch.tensor) -> torch.tensor:
     return torch.zeros_like(tensor, requires_grad=True)
 
 
-def imask_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor, epsilon: float, max_iter=10) -> torch.tensor:
+def imask_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor, epsilon: float, **kwargs) -> torch.tensor:
     """
     Iterative Attack, no target class
     """
@@ -25,24 +27,23 @@ def imask_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tenso
     mask = _create_empty_mask(image)
     opt = optim.SGD([mask], lr=1e-1)
 
-    pbar = tqdm(range(max_iter))
-    for t in pbar:
-
-        pred = model(image + mask)
-        loss = - torch.nn.CrossEntropyLoss()(pred, label)
+    for _ in range(kwargs.get("num_iter", 10)):
 
         opt.zero_grad()
+
+        pred = model(image + mask)
+
+        loss = -torch.nn.CrossEntropyLoss()(pred, label)
         loss.backward()
+
         opt.step()
 
         mask.data.clamp_(-epsilon, epsilon)
 
-        pbar.set_description(f"iter: {t}, loss: {loss.item():.2f}")
-
     return torch.clamp(image + mask, 0, 1)
 
 
-def timask_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor, target: torch.tensor, epsilon: float, epoch=50) -> torch.tensor:
+def timask_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor, target: torch.tensor, epsilon: float, epoch=50, **kwargs) -> torch.tensor:
     """
     TODO
     """
@@ -52,9 +53,7 @@ def timask_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tens
     mask = _create_empty_mask(image)
     opt = optim.SGD([mask], lr=1e-2)
 
-
-    pbar = tqdm(range(epoch))
-    for t in pbar:
+    for _ in range(kwargs.get("num_iter", 10)):
         opt.zero_grad()
 
         pred = model(image + mask)
@@ -68,12 +67,11 @@ def timask_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tens
         opt.step()
 
         mask.data.clamp_(-epsilon, epsilon)
-        pbar.set_description(f"epoch: {t}, loss: {loss.item():.2f}")
 
     return torch.clamp(image + mask, 0, 1)
 
 
-def fgsm_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor, epsilon: float) -> torch.tensor:
+def fgsm_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor, epsilon: float, **kwargs) -> torch.tensor:
     """
     Fast Gradient Sign Method Attack, no target class
     """
@@ -90,7 +88,7 @@ def fgsm_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor
     return torch.clamp(image + epsilon * image.grad.data.sign(), 0, 1)
 
 
-def tfgsm_attack(model: torch.nn.Module, image: torch.tensor, target: torch.tensor, epsilon: float) -> torch.tensor:
+def tfgsm_attack(model: torch.nn.Module, image: torch.tensor, target: torch.tensor, epsilon: float, **kwargs) -> torch.tensor:
     """
     Fast Gradient Sign Method Attack, with target class
     """
@@ -106,7 +104,7 @@ def tfgsm_attack(model: torch.nn.Module, image: torch.tensor, target: torch.tens
     return torch.clamp(image - epsilon * image.grad.data.sign(), 0, 1)
 
 
-def bim_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor, epsilon: float, alpha: float, max_iter=100, stop_threshold=0.01) -> torch.tensor:
+def bim_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor, epsilon: float, alpha: float, max_iter=100, stop_threshold=0.01, **kwargs) -> torch.tensor:
     """
     Basic Iterative Method Attack, no target class
     (Also known as IFGSM)
@@ -119,12 +117,10 @@ def bim_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor,
     if not atk_image.requires_grad:
         atk_image.requires_grad = True
 
-    pbar = tqdm(range(max_iter))
-    for t in pbar:
+    for _ in range(kwargs.get("num_iter", 10)):
         F.nll_loss(model.forward(atk_image), label).backward()
 
         conf = F.softmax(model(atk_image))[0][label]
-        pbar.set_description(f"iter: {t}, conf: {conf.item():.6f}")
 
         if conf.item() < stop_threshold:
             break
@@ -135,7 +131,7 @@ def bim_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor,
     return torch.clamp(atk_image, 0, 1)
 
 
-def tbim_attack(model: torch.nn.Module, image: torch.tensor, target: torch.tensor, epsilon: float, alpha=0.005, max_iter=100, stop_threshold=0.5, exit_when_predicted=True) -> torch.tensor:
+def tbim_attack(model: torch.nn.Module, image: torch.tensor, target: torch.tensor, epsilon: float, alpha=0.005, max_iter=100, stop_threshold=0.5, exit_when_predicted=True, **kwargs) -> torch.tensor:
     """
     Basic Iterative Method Attack, with target class
     """
@@ -147,12 +143,10 @@ def tbim_attack(model: torch.nn.Module, image: torch.tensor, target: torch.tenso
     if not atk_image.requires_grad:
         atk_image.requires_grad = True
 
-    pbar = tqdm(range(max_iter))
-    for t in pbar:
+    for _ in range(kwargs.get("num_iter", 10)):
         output = model.forward(atk_image)
 
         conf = F.softmax(model(atk_image), dim=1)[0][target]
-        pbar.set_description(f"iter: {t}, conf: {conf.item():.6f}")
 
         if conf >= stop_threshold or (exit_when_predicted == True and torch.argmax(model(atk_image), dim=1) == target):
             break
@@ -161,11 +155,10 @@ def tbim_attack(model: torch.nn.Module, image: torch.tensor, target: torch.tenso
 
         atk_image.data = image + torch.clamp((atk_image.data - (alpha * atk_image.grad.data.sign())) - image, -epsilon, epsilon)
 
-
     return torch.clamp(atk_image, 0, 1)
 
 
-def cw_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor, epsilon: float) -> torch.tensor:
+def cw_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor, epsilon: float, **kwargs) -> torch.tensor:
     """
     Carlini & Wagner Attack, no target class
     """
@@ -173,7 +166,7 @@ def cw_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor, 
     pass
 
 
-def df_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor, epsilon: float) -> torch.tensor:
+def df_attack(model: torch.nn.Module, image: torch.tensor, label: torch.tensor, epsilon: float, **kwargs) -> torch.tensor:
     """
     Deepfool Attack, no target class
     """
