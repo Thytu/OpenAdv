145937fc58a30c9cd22c3177bc80e5b131748c54
Thytu <valentin.de-matos@epitech.eu>
2022-01-01 18:31:46 +0100

    feat: data_handler np_array_to_tensor_image

--- src/data_handler.py
+++ src/data_handler.py
@@ -18,5 +18,12 @@ def load_image_to_tensor(image_name, width=IMG_SIZE, height=IMG_SIZE, device='cp
     return image.to(device, torch.float)
 
 
+def np_array_to_tensor_image(img, width=IMG_SIZE, height=IMG_SIZE, device='cpu'):
+    image = Image.fromarray(img).convert('RGB').resize((width, height))
+    image = COMPOSED_TRANSFORMERS(image).unsqueeze(0)
+
+    return image.to(device, torch.float)
+
+
 def tensor_to_image(tensor: torch.tensor) -> Image:
     return transforms.ToPILImage()(tensor.cpu().clone().squeeze(0))
